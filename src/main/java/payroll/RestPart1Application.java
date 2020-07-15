0582bcb1d51f48cd82b9b27fe33d508e290831a1
Samruddhi Kalyankar <samruddhi9369@gmail.com>
2020-07-14 18:58:47 -0700

    Initial Project Version for Non-REST APIS

---
+++ src/main/java/payroll/RestPart1Application.java
@@ -0,0 +1,11 @@
+package payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class RestPart1Application {
+
+	
+
+}
